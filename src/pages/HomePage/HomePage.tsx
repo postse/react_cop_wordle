d6265252187577342f40e1a5339110072975de03
Simon Post <spost@captechventures.com>
2022-07-12 15:34:47 -0400

    can now copy results to clipboard

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import WordleContainer from "../../components/WordleContainer/WordleContainer";
 import words from "../../data/words";
 import JSConfetti from "js-confetti";
@@ -6,12 +6,24 @@ import './HomePage.css'
 
 const HomePage = () => {
     const [lettersTyped, setLettersTyped] = useState<string[]>(["", "", "", "", "", "", "", "", "", "",]);
-    const [word, setWord] = useState<string>(words[Math.floor(Math.random() * words.length)].toUpperCase());
+    const [wordId, setWordId] = useState<number>(Math.floor(Math.random() * words.length));
     const [score, setScore] = useState<number>(0);
     const [hasWon, setHasWon] = useState<boolean>(false);
     const [intervalId, setIntervalId] = useState<any>(0);
 
-    const jsConfetti = new JSConfetti()
+    const [startTime, setStartTime] = useState(0);
+    const [endTime, setEndTime] = useState(0);
+
+    const jsConfetti = new JSConfetti();
+
+    useEffect(() => {
+        if (/^-?\d+$/.test(window.location.pathname.replace("/", ""))) {
+            setWordId(Number(window.location.pathname.replace("/", "")));
+            // console.log(Number(window.location.pathname.replace("/", "")));
+        } else {
+            window.location.pathname = "/" + wordId;
+        }
+    }, [])
 
     const iterateLetter = (event: any) => {
         // checks if the key is a letter
@@ -23,14 +35,18 @@ const HomePage = () => {
                 setLettersTyped(["", "", "", "", "", "", "", "", "", "",]);
                 setScore(0);
                 setHasWon(false);
-                setWord(words[Math.floor(Math.random() * words.length)].toUpperCase());
+                const newWordId = Math.floor(Math.random() * words.length);
+                setWordId(newWordId);
+
+                window.history.replaceState({ additionalInformation: 'Changed URL to new puzzle' }, "", 'http://localhost:3000/' + newWordId)
                 return;
             }
 
-            if (lettersTyped.slice(lettersTyped.length - 5, lettersTyped.length).join("") === word.toUpperCase()) {
+            if (lettersTyped.slice(lettersTyped.length - 5, lettersTyped.length).join("") === words[wordId].toUpperCase()) {
                 setScore(score => score + 10);
                 jsConfetti.addConfetti();
                 setHasWon(true);
+                setEndTime(new Date().getTime())
 
                 if (intervalId !== 0) {
                     clearInterval(intervalId);
@@ -40,7 +56,10 @@ const HomePage = () => {
             } else {
                 setScore(score => score + 10);
 
+                // Runs when game begins
                 if (lettersTyped.length === 11) {
+                    setStartTime(new Date().getTime())
+
                     const updateScoreInterval = setInterval(() => {
                         setScore(score => score + 1);
                     }, 250)
@@ -52,20 +71,32 @@ const HomePage = () => {
 
     return (
         <div className="container" tabIndex={0} onKeyUp={iterateLetter}>
-            <h1>Slidle</h1>
-            {/* <p>The word is {word}</p> */}
+            <h1>Slidle #{wordId}</h1>
+            <p>The word is {words[wordId].toUpperCase()}</p>
             <div className="scoreContainer">
                 <p>Score (lower is better):&nbsp;</p>
                 <strong style={{ "width": score.toString().length + "ch" }}>{score}</strong>
             </div>
-            <WordleContainer word={word} lettersTyped={lettersTyped}></WordleContainer>
+            <WordleContainer word={words[wordId].toUpperCase()} lettersTyped={lettersTyped}></WordleContainer>
             {
                 hasWon &&
-                <div>
-                    <h2>Congrats!</h2>
-                    <p>Your score was {score}.</p>
-                    <p>You typed {lettersTyped.length - 10} letters.</p>
-                    <p>Press any letter to play again.</p>
+                <div className="winContainer">
+                    <h2>You win!</h2>
+                    <p>Score: <strong>{score}</strong></p>
+                    <p>Letters typed: <strong>{lettersTyped.length - 10}</strong></p>
+                    <p>Elapsed time: <strong>{Math.round((endTime - startTime) / 1000)} seconds</strong></p>
+                    <button
+                        className="shareButton"
+                        onClick={() => {
+                            navigator.clipboard.writeText(`
+                            Slidle Game Results!
+                            Total Score: ${score}
+                            Letters typed: ${lettersTyped.length - 10}
+                            Elapsed time: ${Math.round((endTime - startTime) / 1000)} seconds
+                            Play this puzzle: ${window.location.href}
+                        `)
+                        }}>Share results!</button>
+                    <p>Press any letter to play again</p>
                 </div>
             }
         </div>
