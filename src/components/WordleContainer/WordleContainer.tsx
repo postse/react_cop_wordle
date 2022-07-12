f503f10dfe0e4c59737f85ee8df8c09bacb391ab
Simon Post <spost@captechventures.com>
2022-07-12 11:53:05 -0400

    cleaned up wordle tsx code

--- src/components/WordleContainer/WordleContainer.tsx
+++ src/components/WordleContainer/WordleContainer.tsx
@@ -10,45 +10,59 @@ interface WordInfo {
 }
 
 const WordleBoxes = ({ word, lettersTyped }: WordInfo) => {
+
+    const onAppear = (el: any) => {
+        setTimeout(() => {
+            el.classList.add("fadeIn");
+            setTimeout(() => {
+                el.style.opacity = '1';
+                el.classList.remove("fadeIn");
+            }, 100);
+        }, 0);
+    };
+
+    const onExit = (el: any, removeElement: any) => {
+        setTimeout(() => {
+            el.classList.add("fadeOut");
+            setTimeout(removeElement, 50);
+        }, 0);
+    };
+
+    const springConfig = {
+        stiffness: 2000,
+        damping: 80
+    }
+
+    const handleEnterUpdateDelete = ({
+        hideEnteringElements,
+        animateEnteringElements,
+        animateExitingElements,
+        animateFlippedElements
+    }:any) => {
+        hideEnteringElements();
+        animateExitingElements();
+        animateFlippedElements();
+        setTimeout(() => {
+            animateEnteringElements();
+        }, 100);
+    };
+
     return (
         <>
             <p>Word is {word}</p>
             {
-                <Flipper flipKey={lettersTyped.join('')} className="wordleBoxes" spring={{
-                    stiffness: 2000,
-                    damping: 80
-                }} 
-                handleEnterUpdateDelete={({
-                    hideEnteringElements,
-                    animateEnteringElements,
-                    animateExitingElements,
-                    animateFlippedElements
-                }) => {
-                    hideEnteringElements();
-                    animateExitingElements();
-                    animateFlippedElements();
-                    setTimeout(() => {
-                        animateEnteringElements();
-                    }, 100);
-                }}>
+                <Flipper
+                    flipKey={lettersTyped.join('')}
+                    className="wordleBoxes"
+                    spring={springConfig}
+                    handleEnterUpdateDelete={handleEnterUpdateDelete}>
                     {
                         lettersTyped.slice(lettersTyped.length - 10, lettersTyped.length).map((letter, index) => (
-                            <Flipped key={lettersTyped.length + index} flipId={lettersTyped.length + index}
-                                onAppear={(el, i) =>{
-                                    setTimeout(() => {
-                                      el.classList.add("fadeIn");
-                                      setTimeout(() => {
-                                        el.style.opacity = '1';
-                                        el.classList.remove("fadeIn");
-                                      }, 100);
-                                    }, 0);
-                                  }}
-                                onExit={(el, i, removeElement) => {
-                                    setTimeout(() => {
-                                      el.classList.add("fadeOut");
-                                      setTimeout(removeElement, 50);
-                                    }, 0);
-                                  }}>
+                            <Flipped
+                                key={lettersTyped.length + index}
+                                flipId={lettersTyped.length + index}
+                                onAppear={onAppear}
+                                onExit={onExit}>
                                 <div>
                                     <WordleLetterBoxes isActive={index >= 5 ? true : false} correctLetter={word.charAt(index - 5)} actualLetter={letter}></WordleLetterBoxes>
                                 </div>
